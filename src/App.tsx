2f82d3e6b63269e15af3accd644ab5c7c20a342a
miliberlin <michelle.liebheit@posteo.de>
2021-06-21 20:15:30 +0200

    add task to state

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import React, { FC } from 'react';
+import React, { FC, ChangeEvent, useState } from 'react';
 import './App.css';
 import './normalize.css';
+import {ITask} from './Interfaces'
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<string>("");
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {setTask(event.target.value)}
+    else {setDeadline(event.target.value)}
+  }
+
+  const addTask = ():void => {
+    const newTask = {taskName: task, deadline: deadline}
+    setTodoList([...todoList, newTask])
+  }
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
           <div className="inputs">
-            <input type="text" placeholder="Task..." required />
-            <input type="date" />
+            <input type="text" placeholder="Task..." required name="task" onChange={handleChange}/>
+            <input type="date" name="deadline" onChange={handleChange}/>
           </div>
-          <button>Add task</button>
+          <button onClick={addTask}>Add task</button>
         </div>
       </div>
       <div className="todoList"></div>
