7fb82e6014b61e675ac62b14f1fdfebe052f9656
miliberlin <michelle.liebheit@posteo.de>
2021-06-22 20:38:14 +0200

    converted date string to Date object

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,21 @@ import TodoTask from './Components/TodoTask'
 
 const App: FC = () => {
   const [task, setTask] = useState<string>("");
-  const [deadline, setDeadline] = useState<string>("");
+  const [deadline, setDeadline] = useState<Date>(new Date())
   const [todoList, setTodoList] = useState<ITask[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === "task") {setTask(event.target.value)}
     else if (event.target.name === "deadline") {
-      setDeadline(event.target.value)
+      setDeadline(new Date(event.target.value))
     }
   }
 
   const addTask = ():void => {
     const newTask = {taskName: task, deadline: (deadline)}
-    setTodoList([...todoList, newTask]);
+    todoList.length === 0 ? todoList.push({taskName: task, deadline: (deadline)}) : setTodoList([...todoList, newTask]);
     setTask("");
-    setDeadline("");
-    console.log(todoList)
+    setDeadline(new Date());
   }
 
   const completeTask = (taskNameToDelete: string): void => {
@@ -36,7 +35,13 @@ const App: FC = () => {
         <div className="inputContainer">
           <div className="inputs">
             <input type="text" placeholder="Task..." required name="task" value={task} onChange={handleChange}/>
-            <input type="date" name="deadline" onChange={handleChange}/>
+            <input type="date" name="deadline" value={
+              `${deadline.getFullYear()}-${
+              (deadline.getMonth()+1) < 10 ? '0'+(deadline.getMonth()+1) : deadline.getMonth()+1
+            }-${
+              deadline.getDate() < 10 ? '0'+ deadline.getDate() : deadline.getDate()
+            }`
+            } onChange={handleChange}/>
           </div>
           <button onClick={addTask}>Add task</button>
         </div>
@@ -44,11 +49,9 @@ const App: FC = () => {
       <div className="todoList">
         {todoList
         .map((task: ITask, key: number) => {
+          // console.log(task.deadline)
           return <TodoTask key={key} task={task} completeTask={completeTask}/>
         })
-        // .sort((a,b) => {
-        //   return new Date(a.deadline) - new Date(b.deadline)
-        // })
         }
       </div>
     </div>
--- src/Components/TodoTask.tsx
+++ src/Components/TodoTask.tsx
@@ -10,7 +10,13 @@ const TodoTask = ({task, completeTask}: Props) => {
     return (
         <div className="task">
             <div className="taskName">{task.taskName}</div>
-            <div className="taskDeadline">{task.deadline}</div>
+            <div className="taskDeadline">
+                {`${task.deadline.getFullYear()}-${
+              (task.deadline.getMonth()+1) < 10 ? '0'+(task.deadline.getMonth()+1) : task.deadline.getMonth()+1
+            }-${
+                task.deadline.getDate() < 10 ? '0'+ task.deadline.getDate() : task.deadline.getDate()
+            }`}
+            </div>
             <button onClick={() => {completeTask(task.taskName)}}>X</button>
         </div>
     )
